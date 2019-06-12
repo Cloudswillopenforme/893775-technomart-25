debccdd9d3c05792148b24a6e9964d40a9119ce6
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2019-06-12 22:05:37 +0300

    Open modals handlers added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 //open modals
-let page = new Page ({
-  popUpSendEmail: document.querySelector(".pop-up-send-email ");
-  popUpBigMap: document.querySelector(".pop-up-big-map");
+document.body.addEventListener("click", (e) => {
+  let btnSendEmail = e.target.closest('.open-modal');
+
+  if (!btnSendEmail) return;
+
+  let modal = btnSendEmail.dataset.modal;
+
+  openModal(modal);
 });
+
+document.body.addEventListener("click", (e) => {
+  let close = e.target.closest('.pop-up__close');
+
+  if (!close) return;
+
+  let modal = e.target.closest('.pop-up');
+
+  closeModal(modal);
+});
+
+const openModal = (modal) => {
+  document.querySelector(`.${modal}`).classList.remove('visually-hidden');
+};
+
+const closeModal = (modal) => {
+  if (!modal.classList.contains('visually-hidden')) {
+    modal.classList.add('visually-hidden');
+  };
+};
